7394c176351b82fedeaeddf83007c3d4cca2f163
maximoskp <maximoskalpap@gmail.com>
2019-04-13 13:04:43 +0300

    In mnist, saving figs instead of showing.

--- mnist_autoencoder.py
+++ mnist_autoencoder.py
@@ -129,10 +129,10 @@ with tf.Session() as sess:
 
     print("Original Images")
     plt.figure(figsize=(n, n))
-    plt.imshow(canvas_orig, origin="upper", cmap="gray")
-    plt.show()
+    plt.savefig('figs/mnist_test_original.png', dpi=300); plt.clf()
+    # plt.show()
 
     print("Reconstructed Images")
     plt.figure(figsize=(n, n))
-    plt.imshow(canvas_recon, origin="upper", cmap="gray")
-    plt.show()
+    plt.savefig('figs/mnist_test_original.png', dpi=300); plt.clf()
+    # plt.show()
